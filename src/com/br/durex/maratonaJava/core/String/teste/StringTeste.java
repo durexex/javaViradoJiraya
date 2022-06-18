47b98d22feee171060e8557f10ed2032b26b8df1
durexex <eduardodurieuxlopes@gmail.com>
2022-06-17 22:06:52 -0300

    strings

---
+++ src/com/br/durex/maratonaJava/core/String/teste/StringTeste.java
@@ -0,0 +1,14 @@
+package com.br.durex.maratonaJava.core.String.teste;
+
+public class StringTeste {
+    public static void main(String[] args) {
+        String nome = "Gerimun";
+        String nome2 = "Gerimun";
+
+        System.out.println(nome == nome2);
+        System.out.println(nome.equals(nome2));
+
+
+
+    }
+}
---
+++ src/com/br/durex/maratonaJava/core/String/teste/StringTeste02.java
@@ -0,0 +1,14 @@
+package com.br.durex.maratonaJava.core.String.teste;
+
+public class StringTeste02 {
+    public static void main(String[] args) {
+        String nome = "Luffy";
+        System.out.println(nome.charAt(0));
+        System.out.println(nome.length());
+        System.out.println(nome.replace("f", "l"));
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.toUpperCase());
+        System.out.println(nome.substring(0, 2));
+
+    }
+}
